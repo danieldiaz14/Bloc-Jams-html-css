3a093f73cbceb37a92ce6bebcbe484efb6d59d2f
danieldiaz <danieldiaz129@gmail.com>
2017-04-16 20:50:06 -0700

    9 DOM scripting Events Fixed for sure

--- landing.js
+++ landing.js
@@ -1,5 +1,4 @@
-var pointsArray = document.getElementsbyClassName('point');
-
+var pointsArray = document.getElementsByClassName('point');
 
 var revealPoint = function(point) {
     point.style.opacity = 1;
@@ -7,20 +6,18 @@ var revealPoint = function(point) {
     point.style.msTransform = "scaleX(1) translateY(0)";
     point.style.WebkitTransform = "scaleX(1) translateY(0)";
 };
-var animePoints = function(points) {
+
+var animatePoints = function(points) {
     forEach(points, revealPoint);
 };
 
 window.onload = function() {
-var sellingPoints = document.getElementsByClassName('selling-points')[0];
-var scrollDistance = sellingPoints.getBoundingClientRect().top - window.innerHeight + 200;
-    
     if (window.innerHeight > 950) {
         animatePoints(pointsArray);
     }
     
-    window.addEventListener('scroll', function(event) {
-        if (pointsArray[0].getBoundingClientRect.top <= 500) {
+    window.addEventListener("scroll", function(event) {
+        if (pointsArray[0].getBoundingClientRect().top <= 500) {
             animatePoints(pointsArray);
         }
     });
